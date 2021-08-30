49e05289322930261a1e56f9caed2c09a497e701
KydyrovErjan <erjankydyrov22@gmail.com>
2021-08-30 16:11:37 +0600

    Products can really edit

--- products/models.py
+++ products/models.py
@@ -5,17 +5,29 @@ User = get_user_model()
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=100,)
     description = models.TextField()
-    resto = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products',
                               null=True,
                               blank=True)
+    likes = models.DecimalField(max_digits=10, decimal_places=0, null=True)
 
     class Meta:
         ordering = ['title', 'price']
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
+
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(Product,
+                                on_delete=models.CASCADE,
+                                related_name='reviews')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='reviews')
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
 
--- products/views.py
+++ products/views.py
@@ -1,15 +1,30 @@
-from django.http import HttpResponse
-from django.views import View
 from rest_framework import viewsets, mixins
-from rest_framework.decorators import api_view, action
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, RetrieveAPIView, \
-    CreateAPIView, UpdateAPIView, DestroyAPIView
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+
 from django_filters import rest_framework as filters
 from rest_framework import filters as rest_filters
+from .models import Product, ProductReview
+from .serializers import ProductSerializer, CreateProductSerializer, ProductDetailsSerializer, ReviewSerializer
 
 
-def test_view(request):
-    return HttpResponse('Hello world!')
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    filter_backends = [filters.DjangoFilterBackend,
+                       rest_filters.SearchFilter,
+                       rest_filters.OrderingFilter]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ProductSerializer
+        elif self.action == 'retrieve':
+            return ProductDetailsSerializer
+        return CreateProductSerializer
+
+
+class ReviewViewSet(mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    viewsets.GenericViewSet):
+    queryset = ProductReview.objects.all()
+    serializer_class = ReviewSerializer
+
+
