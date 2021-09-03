02e18c69c2d6f9ac1a71d6fe772e531845f4bd4e
KydyrovErjan <erjankydyrov22@gmail.com>
2021-09-03 19:53:44 +0600

    swager

--- restobar/settings.py
+++ restobar/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
-    'rest_framework_swagger'
+    'drf_yasg',
 
     'users',
     'products',
--- restobar/urls.py
+++ restobar/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
-
 from products.views import ProductViewSet, ReviewViewSet, RestaurantViewSet, FavouriteViewSet, LikeViewSet
 
 
@@ -27,9 +29,24 @@ router.register('restaurants', RestaurantViewSet)
 router.register('favourite', FavouriteViewSet)
 router.register('like', LikeViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='python_shop',
+        default_version='v1',
+        description='Test description',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='contact@snippets.local'),
+        license=openapi.License(name='BSD License')
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
+
 
 urlpatterns = [
-    path('', include('users.urls')),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger',cache_timeout=0)),
+
 ]
